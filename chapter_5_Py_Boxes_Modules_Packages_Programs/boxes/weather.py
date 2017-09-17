79797030092f7e374adc62cfd2f881808d838004
henry199101 <331243543@qq.com>
2017-09-17 11:39:23 +0800

    commit files in boxes

---
+++ chapter_5_Py_Boxes_Modules_Packages_Programs/boxes/weather.py
@@ -0,0 +1,6 @@
+from sources import daily, weekly
+
+print("daily forecast:", daily.forecast())
+print("weekly forecast:")
+for number, outlook, in enumerate(weekly.forecast(), 1):
+	print(number, outlook)

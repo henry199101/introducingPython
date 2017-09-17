f1b2092c0eef44a9ffaae44e674a34d79467679b
henry199101 <331243543@qq.com>
2017-09-17 15:40:56 +0800

    commit usePropertyDemo.py

---
+++ chapter_6_object_class/usePropertyDemo.py
@@ -0,0 +1,17 @@
+class Duck():
+	def __init__(self, input_name):
+		self.hidden_name = input_name
+	def get_name(self):
+		print('inside the getter')
+		return self.hidden_name
+	def set_name(self, input_name):
+		print('inside the getter')
+		self.hidden_name = input_name
+	name = property(get_name, set_name)
+
+fowl = Duck('Howard')
+print(fowl.name)
+print(fowl.get_name())
+
+fowl.name = 'MIT'
+print(fowl.name)
